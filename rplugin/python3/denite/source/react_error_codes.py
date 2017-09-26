ea49b6db9259b2b8c80efc7bd0f7907c1a5a01c3
年糕小豆汤 <ooiss@qq.com>
2017-09-26 22:47:08 +0800

    press Enter key to open the document on browser

--- rplugin/python3/denite/source/react_error_codes.py
+++ rplugin/python3/denite/source/react_error_codes.py
@@ -8,10 +8,17 @@
 
 import os
 
+import webbrowser
+
+from ..kind.base import Base as BaseKind
+
 from .base import Base
 
+# 脚本所在目录绝对路径
 CURRENT_SCRIPT_PATH_DIRNAME = os.path.dirname(os.path.abspath(__file__))
 
+# react error code 的文档路径
+CODE_DOCUMENT_URI = 'https://facebook.github.io/react/docs/error-decoder.html?invariant=%s'
 
 class Source(Base):
 
@@ -19,13 +26,26 @@ class Source(Base):
         super().__init__(vim)
 
         self.name = 'react_error_codes'
-        self.kind = 'file'
         self.matchers = ['matcher_regexp']
         self.sorters = []
+        self.kind = Kind(vim)
 
     def on_init(self, context):
-        context['codes'] = [{'word': line.strip()}
+        context['codes'] = [{'word': line.strip(),
+                            'source__code': line.split(':')[0]}
                 for line in open(os.path.join(CURRENT_SCRIPT_PATH_DIRNAME, 'codes.dat'), 'r')]
 
     def gather_candidates(self, context):
         return context['codes']
+
+class Kind(BaseKind):
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.default_action = 'open'
+        self.persist_actions += ['open']
+        self.redraw_actions += ['open']
+        self.name = 'react_error_codes'
+
+    def action_open(self, context):
+        webbrowser.open(CODE_DOCUMENT_URI % context['targets'][0]['source__code'])
